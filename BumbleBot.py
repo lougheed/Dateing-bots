88c25ef82dcc76a378ac851428a8e53f2bc57ce5
DeCyberToaster <36155805+lougheed@users.noreply.github.com>
2020-06-18 19:25:21 +0100

    Update BumbleBot.py
    Just a tidy up of the code and removing some of the spacing.

--- BumbleBot.py
+++ BumbleBot.py
@@ -1,16 +1,11 @@
 from selenium import webdriver
-from time import sleep 
-
-#both of these are needed for the GUI
-
+from time import sleep # both of these are needed to get things working. 
 
 #For the script to work you need to enter your username and password in the code below so it can login you in.
-
 
 class BumbleBot():#Setting a main class item
     def __init__(self):
         self.driver = webdriver.Chrome("chromedriver.exe")#Finds the chromedriver, this should be in the same folder as this code on your PC
-
          
     def login(self):#Handles login
         self.driver.get("https://bumble.com/app")#Opens the webpage
@@ -23,9 +18,8 @@ class BumbleBot():#Setting a main class item
         # switch to login popup
         base_window = self.driver.window_handles[0]
         self.driver.switch_to_window(self.driver.window_handles[1])
-
+        
         #Find the username & password field enters strings(plan text passwords) and 
-      
         email_in = self.driver.find_element_by_xpath('//*[@id="email"]')
         email_in.send_keys("Your username here")
         
@@ -46,8 +40,7 @@ class BumbleBot():#Setting a main class item
     def like(self):#Find the like button and click 
         like_btn = self.driver.find_element_by_xpath('//*[@id="main"]/div/div[1]/main/div[2]/div/div/span/div[2]/div/div[2]/div/div[3]/div')
         like_btn.click()
-  
-    
+     
     #Lots going on here, will brake it down line by line 
     def swipe(self):#Main mothod
         import random#Need the random library
@@ -75,7 +68,6 @@ class BumbleBot():#Setting a main class item
                if Number_Of_Likes_count ==3:#every 15 or so likes check to see if the we have a hit and send a few msgs 
                    Log_file.close()
                    self.Messege_bot()
-                   
                    continue                    
                    
                if Number_Of_Likes_count == 30:#Caps the whole loop at 2 likes, tweak as you need 
@@ -94,28 +86,14 @@ class BumbleBot():#Setting a main class item
                 Log_file.close()
                 self.driver.close()
                 exit()
-                
-               
+                               
     def Messege_bot(self):
             #Test code will add a working function soon 
             print("Test")
             self.swipe()
-            
-                            
+                                        
 #Calls  everything we need form the above class         
 Bot = BumbleBot()
 Bot.login()
 sleep(2)
 Bot.swipe()
-
-
-
-        
-        
-
-        
-        
-        
-
-
-
